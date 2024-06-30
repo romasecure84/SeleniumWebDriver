489730704e4aa9fbdf7ff6a00904e0c89244fcab
ROMA2 <romasecure84@yandex.ru>
2024-06-30 22:13:14 +0400

    Created FindByElementsTag class

---
+++ src/Selenium/FindByElementsTag.java
@@ -0,0 +1,25 @@
+package Selenium;
+
+import Utilities.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class FindByElementsTag extends BaseDriver {
+    public static void main(String[] args) {
+        driver.get("https://www.hepsiburada.com/");
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for (WebElement e : links) {
+            if (!e.getText().isEmpty()) {
+                System.out.println("e.getAttribute('href') = " + e.getAttribute("href"));
+                System.out.println("e.getAttribute('title') = " + e.getAttribute("title"));
+                System.out.println("e.getAttribute('rel') = " + e.getAttribute("rel"));
+            }
+        }
+
+        WaitQuit();
+    }
+}
